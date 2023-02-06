6cacc6567b1c0b76e81bb6070089d8dcb5ba239b
Adrian Legaspi <legaspi.adrianking@gmail.com>
2023-02-06 19:33:28 +0200

    fix shit

--- src/main/scala/gg/fence/data/MongoClient.scala
+++ src/main/scala/gg/fence/data/MongoClient.scala
@@ -1,10 +1,9 @@
 package gg.fence.data
 import akka.contrib.persistence.mongodb.Bson
-import gg.fence.data.MongoClient.{JsTransformer, JsValue}
+import gg.fence.data.MongoClient.JsTransformer
 import org.mongodb.scala.bson.{BsonDocument, BsonValue}
-// import org.bson.{BsonArray, BsonBoolean, BsonInt64, BsonNull, BsonObjectId, BsonString, BsonValue}
 import org.mongodb.scala._
-import org.mongodb.scala.bson.{BsonArray, BsonBoolean, BsonDecimal128, BsonObjectId, BsonString, BsonNull}
+import org.mongodb.scala.bson.{BsonArray, BsonBoolean, BsonDecimal128, BsonString, BsonNull}
 import org.mongodb.scala.{Observer => DbObserver}
 
 import scala.concurrent.{ExecutionContext, Future, Promise}
@@ -32,8 +31,8 @@ object MongoClient {
    * @tparam A the type to be transformed
    */
   trait JsTransformer[A] {
-    def toJsValue(data: A): JsValue
-    def fromJsValue(data: JsValue): A
+    def toJsValue(data: A): JsObject
+    def fromJsValue(data: JsObject): A
   }
 
 
@@ -65,12 +64,7 @@ object MongoClient {
 
   def apply(client: org.mongodb.scala.MongoClient, collectionRegistry: Map[Class[_],String])(implicit ec: ExecutionContext): MongoClient = new MongoClient {
 
-    // what the fuck does this mean
-    // something something Atlas
-    // System.setProperty("org.mongodb.async.type", "netty")
-
-    val db: MongoDatabase = client.getDatabase("fence-db")
-
+    private val db: MongoDatabase = client.getDatabase("fence-db")
 
     /** Converts a [[BsonValue]] to a [[JsValue]]
      *  If it encounters a [[org.mongodb.scala.bson.BsonArray]] or [[org.mongodb.scala.bson.BsonDocument]]
@@ -103,25 +97,38 @@ object MongoClient {
       case JsNumber(value) => BsonDecimal128(value)
       case JsBoolean(value) => BsonBoolean(value)
       case JsArray(value) => BsonArray.fromIterable(value.map(fromJsValue))
-      case JsObject(objectValue) => BsonDocument(objectValue.map{ case (key, value) =>
-        key -> fromJsValue(value)
-      })
+      case jsObj: JsObject => fromJsObject(jsObj)
+    }
+
+    private def fromJsObject(jsObject: JsObject): BsonDocument = {
+      val jsValueMap = jsObject.value.map { case (key, jsValue) =>
+        key -> (jsValue match {
+          case obj: JsObject => fromJsObject(obj)
+          case other => fromJsValue(other)
+        })
+      }
+      BsonDocument(jsValueMap)
+    }
+
+    private def observeResults(collection: MongoCollection[Document]): Future[List[Document]] = {
+      val promise: Promise[Seq[Document]] = Promise()     // Create a Promise to be used in the Observer
+      val observer = new Observer[Document](promise)      // Create the Observer that subscribes to the collection Observable
+      collection.find().subscribe(observer)               // Make the observer subscribe to the documents emitted by the observable
+      promise.future.map(_.toList)
     }
 
 
     override def getData[A]()(implicit transformer:  JsTransformer[A]): Future[List[A]] = {
-      val promise: Promise[Seq[Document]] = Promise()                              // Create a Promise to be used in the Observer
-      val observer = new Observer[Document](promise)                               // Create the Observer that subscribes to the collection Observable
       val collectionName = collectionRegistry(classOf[A])                          // Get the collection name of the data we are getting from the registry
-      val collection: MongoCollection[Document] = db.getCollection(collectionName) // Get the collection
-      collection.find().subscribe(observer)                                        // Make the observer subscribe to the documents emitted by the observable
-      promise.future.map { listOfDocument =>
+      val mongoCollection = db.getCollection(collectionName)                       // Get the Mongo Collection for the data we are getting
+      val result = observeResults(mongoCollection)
+      result.map { listOfDocument =>
         listOfDocument.map { document =>
           val bsonJsValue = JsObject(document.map { case (key, value) =>           // Iterate on the BsonValue the client gives us
             key -> toJsValue(value)                                                // Transform Java's bullshit BsonValue into something useful in our application which is a JsValue
           }.toMap)                                                                 // Since Document is an Iterable of Key-Value pair, it's a Map and we transform it into a Map
           transformer.fromJsValue(bsonJsValue)                                     // Transform JsValue to an A
-        }.toList                                                                   // Convert the Sequence of A to a List of A
+        }
       }
     }
 
@@ -130,15 +137,14 @@ object MongoClient {
       val observer = new Observer[Document](promise)
       val collectionName = collectionRegistry(classOf[A])
       val collection: MongoCollection[Document] = db.getCollection(collectionName)
+      collection.find()
     }
 
     override def addData[A](data: A)(implicit transformer: JsTransformer[A]): Future[Unit] = {
-      val promise: Promise[Seq[Document]] = Promise()
-      val observer = new Observer[Document](promise)
       val collectionName = collectionRegistry(classOf[A])
-      val collection: MongoCollection[Document] = db.getCollection(collectionName)
-      val bsonData = fromJsValue(transformer.toJsValue(data))
-      collection.insertOne(bsonData)
+      val jsValueOfData = transformer.toJsValue(data)
+      val bsonDocument = fromJsObject(jsValueOfData)
+      db.getCollection(collectionName).insertOne(bsonDocument).toFuture().map(_ => ())
     }
   }
 }
